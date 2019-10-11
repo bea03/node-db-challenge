fb968cc5bc62130a4bf73bfea008592e289bc8c4
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-11 12:01:39 -0600

    seeding works

--- data/seeds/002-resourceSeeds.js
+++ data/seeds/002-resourceSeeds.js
@@ -1,15 +1,15 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('resources').del()
-    .then(function () {
+  return knex("resources")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('resources').insert([
-        {name: 'rowValue1', description: 'This is a description1'},
-        {name: 'rowValue2', description: 'This is a description2'},
-        {name: 'rowValue3', description: 'This is a description3'},
-        {name: 'rowValue4', description: 'This is a description4'},
-        {name: 'rowValue5', description: 'This is a description5'}
+      return knex("resources").insert([
+        { name: "rowValue1", description: "This is a description1" },
+        { name: "rowValue2", description: "This is a description2" },
+        { name: "rowValue3", description: "This is a description3" },
+        { name: "rowValue4", description: "This is a description4" },
+        { name: "rowValue5", description: "This is a description5" }
       ]);
     });
 };
