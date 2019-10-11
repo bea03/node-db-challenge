9c23846482b88a4189181c91018f420153671028
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-11 10:20:53 -0600

    working on tables still

--- knexfile.js
+++ knexfile.js
@@ -15,5 +15,37 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
   }
 };
